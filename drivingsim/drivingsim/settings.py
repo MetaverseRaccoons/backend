45f8a0a7c1cf689a1879690f2e29e0a6a372c860
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2022-12-12 15:10:17 +0100

    Change view methods

--- drivingsim/drivingsim/settings.py
+++ drivingsim/drivingsim/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,6 +77,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 WSGI_APPLICATION = 'drivingsim.wsgi.application'
