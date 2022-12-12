fedd9557d821bb044a49a0e832a51583bd431978
Kevin Oyen <kevin.oyen@student.kuleuven.be>
2022-12-12 16:36:57 +0100

    register user

--- drivingsim/backend/views.py
+++ drivingsim/backend/views.py
@@ -3,9 +3,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer
+from rest_framework import serializers, generics, status
+import uuid
+from .models import User
 
 
-class UserView(APIView):
+
+class UserView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    
     def get_permissions(self):
         if self.request.method == 'POST':
             permission_classes = [AllowAny]
@@ -17,6 +23,16 @@ class UserView(APIView):
         serializer = UserSerializer(request.user)
         return JsonResponse(serializer.data)
 
-    def post(self, request):
-        print("test")
-        return Response("test")
+    def post(self, request):    
+        serializer = self.get_serializer(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response({
+                "RequestId": str(uuid.uuid4()),
+                "Message": "User created successfully",
+                "User": serializer.data}, status=status.HTTP_201_CREATED,
+                )
+        
+        return Response({"Errors": serializers.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        
