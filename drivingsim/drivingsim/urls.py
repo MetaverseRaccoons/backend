4b41856cddc34be4c98c3f7af4ee5cfd0b4ae9dc
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2022-12-20 19:01:51 +0100

    Use more idiomatic REST-style URLs

--- drivingsim/drivingsim/urls.py
+++ drivingsim/drivingsim/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/user/send_friend_request/<int:userID>/', views.FriendsView.send_friend_request, name='friend_request'),
-    path('api/user/accept_friend_request/<int:requestID>/', views.FriendsView.accept_friend_request, name='accept_friend_request'),
-    path('api/user/decline_friend_request/<int:requestID>/', views.FriendsView.decline_friend_request, name='decline_friend_request'),
+    path('api/friend/<str:username>/send', views.send_friend_request, name='friend_request'),
+    path('api/friend/<str:username>/accept', views.accept_friend_request, name='accept_friend_request'),
+    path('api/friend/', views.list_friends, name='list_friends'),
 ]
 
