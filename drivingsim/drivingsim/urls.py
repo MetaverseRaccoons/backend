c7053b0cbb38564ec37cba51c8e049d3eec4bddb
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2023-02-15 16:12:26 +0100

    New endpoints for friends

--- drivingsim/drivingsim/urls.py
+++ drivingsim/drivingsim/urls.py
@@ -14,8 +14,9 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/friend/<str:username>/send', views.send_friend_request, name='friend_request'),
-    path('api/friend/<str:username>/accept', views.accept_friend_request, name='accept_friend_request'),
-    path('api/friend/', views.list_friends, name='list_friends'),
+    path('api/friend/request/<str:to_username>/send/', views.send_friend_request, name='friend_request'),
+    path('api/friend/request/<str:from_username>/accept/', views.accept_friend_request, name='accept_friend_request'),
+    path('api/friend/request/', views.friend_requests, name='friend_requests'),
+    path('api/friend/all/', views.all_friends, name='all_friends'),
 ]
 
