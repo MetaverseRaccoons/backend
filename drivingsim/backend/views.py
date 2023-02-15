228fcba129951a5f6531d964f71eb859c2ceaef6
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2023-02-15 16:12:16 +0100

    Create new endpoints for friend related functions, change all POST properties to data (so we always use JSON)

--- drivingsim/backend/views.py
+++ drivingsim/backend/views.py
@@ -8,7 +8,6 @@ from .serializers import UserSerializer, FriendsSerializer
 from rest_framework import generics, status
 from .models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from django.contrib.auth import authenticate, login
 
@@ -32,14 +31,14 @@ class UserView(generics.GenericAPIView):
             try:
                 user = User.objects.get(username=username)
             except User.DoesNotExist:
-                return JsonResponse({'error': "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+                return JsonResponse({'message': "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
             if not user.is_shareable and username is not None:
-                return JsonResponse({'error': "User is private"}, status=status.HTTP_403_FORBIDDEN)
+                return JsonResponse({'message': "User is private"}, status=status.HTTP_403_FORBIDDEN)
         serializer = UserSerializer(user)
         return JsonResponse(serializer.data)
 
     def post(self, request):
-        form = CreateUserForm(request.POST)
+        form = CreateUserForm(request.data)
         if form.is_valid():
             user = form.save()
             refresh = RefreshToken.for_user(user)
@@ -59,15 +58,15 @@ class PasswordView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        username = request.POST['username']
-        old_password = request.POST['old_password']
+        username = request.data['username']
+        old_password = request.data['old_password']
         user = authenticate(username=username, password=old_password)
         if user is not None:
             login(request, user)
-            user.set_password(request.POST['new_password'])
+            user.set_password(request.data['new_password'])
             user.save()
-            return Response("Password changed successfully", status=status.HTTP_200_OK)
-        return Response("failed to change password", status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({'message': "Password changed successfully"}, status=status.HTTP_200_OK)
+        return JsonResponse({'message': "Failed to change password"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
@@ -77,18 +76,18 @@ def send_friend_request(request, to_username):
     try:
         to_user = User.objects.get(username=to_username)
     except User.DoesNotExist:
-        return JsonResponse({'error': "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'message': "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
     if to_user.get_friend_requests().filter(from_user=from_user).exists():
-        return JsonResponse({'error': "Friend request already sent"}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'message': "Friend request already sent"}, status=status.HTTP_400_BAD_REQUEST)
 
     if from_user.get_friend_requests().filter(to_user=to_user).exists():
-        return JsonResponse({'error': "Friend request already received"}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'message': "Friend request already received"}, status=status.HTTP_400_BAD_REQUEST)
 
-    friends = Friends.objects.get_or_create(from_user=from_user, to_user=to_user)
+    friends = Friends(from_user=from_user, to_user=to_user, accepted=False)
     friends.save()
 
-    return JsonResponse({'success': "Friend request sent"}, status=status.HTTP_200_OK)
+    return JsonResponse({'message': "Friend request sent"}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -98,12 +97,12 @@ def accept_friend_request(request, from_username):
     try:
         from_user = User.objects.get(username=from_username)
     except User.DoesNotExist:
-        return JsonResponse({'error': "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'message': "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
     try:
         friends = Friends.objects.get(from_user=from_user, to_user=to_user)
     except Friends.DoesNotExist:
-        return JsonResponse({'error': "Friend request does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'message': "Friend request does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
     friends.accepted = True
     friends.save()
@@ -113,9 +112,18 @@ def accept_friend_request(request, from_username):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def list_friends(request):
+def friend_requests(request):
     user = request.user
-    friends = user.list_friends()
+    friend_requests = user.get_friend_requests()
+    serializer = FriendsSerializer(friend_requests, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def all_friends(request):
+    user = request.user
+    friends = user.get_friends()
     serializer = FriendsSerializer(friends, many=True)
-    return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
